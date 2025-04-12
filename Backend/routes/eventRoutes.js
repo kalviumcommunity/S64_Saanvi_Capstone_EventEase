e323e5c0421d20e174f9ffdd1b03323134492a7c
btw-saanvi <Saanvigarg2020@gmail.com>
2025-04-12 14:44:19 +0530

    get endpoint added

--- Backend/routes/eventRoutes.js
+++ Backend/routes/eventRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Event = require('../models/Event');
+const Event = require("../models/event");
+const { createEvent } = require('../controllers/eventController');
 
-// GET /api/events
+// GET /api/events (Fetch all events)
 router.get('/', async (req, res) => {
   try {
     const events = await Event.find();
@@ -11,5 +12,8 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// POST /api/events (Create a new event)
+router.post('/', createEvent);
 
 module.exports = router;
