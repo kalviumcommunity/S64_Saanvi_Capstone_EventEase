71b756589c08f915e2027a37f4f82e8fb6b8592d
btw-saanvi <Saanvigarg2020@gmail.com>
2025-08-05 23:49:46 +0530

    Update Login.jsx

--- Frontend/client/src/pages/Login.jsx
+++ Frontend/client/src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
         username: formData.username,
         password: formData.password,
       });
